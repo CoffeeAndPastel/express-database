a326c897e5158a9ab18ab14b7f1a2343047f2129
MuffinPlease <benjamintec3455@gmail.com>
2022-07-17 12:54:02 -0500

    Class 7 Env variables

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,21 @@
-const {Pool} = require("pg")
+const {Pool} = require("pg");
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'admin',
-  password: 'admin123',
-  database: 'my_store'
-});
+const {config} = require('./../config/config');
+
+//Secure connection
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgress://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
+
+//Normal connection
+// const pool = new Pool({
+  //   host: 'localhost',
+  //   port: 5432,
+  //   user: 'admin',
+  //   password: 'admin123',
+  //   database: 'my_store'
+  // });
