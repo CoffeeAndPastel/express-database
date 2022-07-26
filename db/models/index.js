8b169662fdfc236284b6c4ea2da6db0d57d050a4
MuffinPlease <benjamintec3455@gmail.com>
2022-07-25 20:45:58 -0500

    Class 16 Relations

--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,13 @@
 const {UserSchema, User} = require('./user.model')
+const {CustomerSchema, Customer} = require('./customer.model')
 
 function setupModels(sequelize){
+  //Init
   User.init(UserSchema, User.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
+
+  //Association
+  Customer.associate(sequelize.models)
 }
 
 module.exports = setupModels;
