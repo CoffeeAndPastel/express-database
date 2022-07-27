797b7d0e2dcba8dd2724d85f7a941d1cfc1b7820
MuffinPlease <benjamintec3455@gmail.com>
2022-07-27 13:10:26 -0500

    Class 21

--- db/models/index.js
+++ db/models/index.js
@@ -3,6 +3,7 @@ const {CustomerSchema, Customer} = require('./customer.model')
 const {CategorySchema, Category} = require('./category.model')
 const {ProductSchema, Product} = require('./product.model');
 const { Order, OrderSchema } = require('./order.model');
+const { OrderProduct, OrderProductSchema } = require('./order-product.model');
 
 function setupModels(sequelize){
   //Init
@@ -11,12 +12,14 @@ function setupModels(sequelize){
   Category.init(CategorySchema, Category.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
   Order.init(OrderSchema, Order.config(sequelize));
+  OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize));
 
   //Association
   User.associate(sequelize.models);
   Customer.associate(sequelize.models);
   Category.associate(sequelize.models);
   Order.associate(sequelize.models);
+  // OrderProduct.associate(sequelize.models);
 }
 
 module.exports = setupModels;
---
+++ db/models/order-product.model.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const { ORDER_TABLE } = require('./order.model');
+const { PRODUCTS_TABLE } = require('./product.model');
+
+const ORDER_PRODUCT_TABLE = 'orders_has_products';
+
+const OrderProductSchema =  {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+  orderId: {
+    field: 'order_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: ORDER_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
+  },
+  amount: {
+    defaultValue: 1,
+    type: DataTypes.INTEGER
+  },
+  productId: {
+    field: 'product_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: PRODUCTS_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
+  },
+}
+
+class OrderProduct extends Model {
+
+  static associate() {
+
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: ORDER_PRODUCT_TABLE,
+      modelName: 'OrderProduct',
+      timestamps: false
+    }
+  }
+}
+
+module.exports = { OrderProduct, OrderProductSchema, ORDER_PRODUCT_TABLE };
