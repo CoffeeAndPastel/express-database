e470da0306ad2c1d4eac0810e8e1542d99da019c
MuffinPlease <benjamintec3455@gmail.com>
2022-07-27 09:09:51 -0500

    Class 18

---
+++ db/migrations/20220727140310-products.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { CATEGORY_TABLE, CategorySchema } = require("../models/category.model");
+const { PRODUCTS_TABLE, ProductSchema } = require("../models/product.model");
+
+
+
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
+    await queryInterface.createTable(PRODUCTS_TABLE, ProductSchema);
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable(CATEGORY_TABLE);
+    await queryInterface.dropTable(PRODUCTS_TABLE);
+  }
+};
