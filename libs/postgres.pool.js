88effa2d410424a295044e690be7c3e10ca5c9ba
MuffinPlease <benjamintec3455@gmail.com>
2022-07-28 10:50:28 -0500

    Fix error in sequelize

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,15 +1,8 @@
-const {Pool} = require("pg");
-
-const {config} = require('./../config/config');
-
 //Secure connection
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const USER = encodeURIComponent(config.dbUser);
+// const PASSWORD = encodeURIComponent(config.dbPassword);
+// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const pool = new Pool({ connectionString: URI });
-
-module.exports = pool;
 
 //Normal connection
 // const pool = new Pool({
@@ -19,3 +12,25 @@ module.exports = pool;
   //   password: 'admin123',
   //   database: 'my_store'
   // });
+
+const { Pool } = require('pg');
+
+const { config } = require('./../config/config');
+
+const options = {};
+
+if (config.isProd) {
+  options.connectionString = config.dbUrl;
+  options.ssl =  {
+    rejectUnauthorized: false
+  };
+} else {
+  const USER = encodeURIComponent(config.dbUser);
+  const PASSWORD = encodeURIComponent(config.dbPassword);
+  const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+  options.connectionString = URI;
+}
+
+const pool = new Pool(options);
+
+module.exports = pool;
